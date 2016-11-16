94cb39a8d83c1769e13f7e3d7ecc1ad794c8bbfe
kryo4096 <kryo4096@gmail.com>
2016-11-16 09:26:17 +0100

    Add Noisemap Generation

--- src/common/Noise.java
+++ src/common/Noise.java
@@ -1,8 +1,29 @@
 package common;
 
+import java.util.Random;
+
 /**
  * Created by ForcedWindows1 on 16.11.2016.
  */
 public class Noise {
 
+    public float[][] generateFlatNoiseMap(int w, int h, NoiseType type, int seed){
+        Random random = new Random(seed);
+        float[][] map = new float[w][h];
+        for(int x = 0; x<w;x++) {
+            for(int y = 0; y<h;y++) {
+                switch (type) {
+                    case RANDOM:
+                        map[w][h] = random.nextFloat()*2-1;
+                        break;
+                    case GAUSSIAN:
+                        map[w][h] = (float) random.nextGaussian()*2-1;
+                        break;
+                }
+            }
+        }
+
+        return map;
+
+    }
 }
