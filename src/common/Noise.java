272d60a04f4adf7cddb5754d50561e40b75b028e
kryo4096 <kryo4096@gmail.com>
2016-11-16 13:24:31 +0100

    Remove NoiseType, prng always gaussian

--- src/common/Noise.java
+++ src/common/Noise.java
@@ -3,23 +3,16 @@ package common;
 import java.util.Random;
 
 /**
- * Created by ForcedWindows1 on 16.11.2016.
+ * Created by kryo4096 on 16.11.2016.
  */
 public class Noise {
 
-    public float[][] generateFlatNoiseMap(int w, int h, NoiseType type, int seed){
+    public float[][] generateFlatNoiseMap(int w, int h, int seed){
         Random random = new Random(seed);
         float[][] map = new float[w][h];
         for(int x = 0; x<w;x++) {
             for(int y = 0; y<h;y++) {
-                switch (type) {
-                    case RANDOM:
-                        map[w][h] = random.nextFloat()*2-1;
-                        break;
-                    case GAUSSIAN:
-                        map[w][h] = (float) random.nextGaussian()*2-1;
-                        break;
-                }
+                map[w][h] = (float) random.nextGaussian()*2-1;
             }
         }
 
--- src/common/NoiseType.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-/**
- * Created by ForcedWindows1 on 16.11.2016.
- */
-public enum NoiseType {
-    RANDOM, GAUSSIAN;
-}
